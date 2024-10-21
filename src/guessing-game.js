3df750d466331a07c6dc19581175e29e62cfbfb4
KateGomel <ykatuay@gmail.com>
2024-10-21 19:57:04 +0300

    feat: complected task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min;
+    this.max;
+    this.mid;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.mid = Math.ceil((this.min + this.max) / 2);
+    return this.mid;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.mid;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.mid;
+  }
 }
 
 module.exports = GuessingGame;
